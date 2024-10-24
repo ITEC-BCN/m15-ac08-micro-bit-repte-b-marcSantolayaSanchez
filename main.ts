e961de132d6d488334f8718a43c354d49f61542d
marcSantolayaSanchez <146827090+marcSantolayaSanchez@users.noreply.github.com>
2024-10-24 17:22:09 +0200

    SUENA CANCION MIA Y MELODIA

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    music.play(music.stringPlayable("C D E F G A B C5 ", 120), music.PlaybackMode.UntilDone)
+})
+input.onButtonPressed(Button.B, function () {
+    music.play(music.stringPlayable("- G E B E - E - ", 120), music.PlaybackMode.UntilDone)
 })
