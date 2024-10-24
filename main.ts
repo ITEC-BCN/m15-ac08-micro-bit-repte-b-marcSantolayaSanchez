e13e1160ca9963f222b75a6c7b68bd5895069192
marcSantolayaSanchez <146827090+marcSantolayaSanchez@users.noreply.github.com>
2024-10-24 17:23:43 +0200

    TEMPO

--- main.ts
+++ main.ts
@@ -2,5 +2,5 @@ input.onButtonPressed(Button.A, function () {
     music.play(music.stringPlayable("C D E F G A B C5 ", 120), music.PlaybackMode.UntilDone)
 })
 input.onButtonPressed(Button.B, function () {
-    music.play(music.stringPlayable("- G E B E - E - ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("- G E B E - E - ", 40), music.PlaybackMode.UntilDone)
 })
